c31b5648fb597acedcce848100885e904d797a51
Dulaul <dennis-ionut.radoi@s.unibuc.ro>
2026-03-23 10:53:00 +0200

    initial

---
+++ src/com/pao/laboratory05/playlist/Song.java
@@ -0,0 +1,7 @@
+package com.pao.laboratory05.playlist;
+
+public record Song(String title, String artist, int durationSeconds)
+        implements Comparable<Song> {
+    // compareTo: sortare după titlu (alfabetic)
+    // Hint: String are deja compareTo — folosește-l
+}
