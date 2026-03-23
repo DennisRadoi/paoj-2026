735e67e1a175985ecf62540ba640aec47b5799bf
Dulaul <dennis-ionut.radoi@s.unibuc.ro>
2026-03-23 12:47:24 +0200

    ex1

--- src/com/pao/laboratory05/playlist/Main.java
+++ src/com/pao/laboratory05/playlist/Main.java
@@ -1,4 +1,6 @@
 package com.pao.laboratory05.playlist;
+import com.pao.laboratory05.playlist.Song;
+import com.pao.laboratory05.playlist.Playlist;
 
 /**
  * Exercise 1 — Playlist muzică
@@ -12,5 +14,20 @@ package com.pao.laboratory05.playlist;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Cerințele se află în Readme.md — secțiunea Exercise 1.");
+
+        Playlist playlist = new Playlist("Road Trip");
+        playlist.addSong(new Song("Waterloo", "ABBA", 174));
+        playlist.addSong(new Song("Bohemian Rhapsody", "Queen", 354));
+        playlist.addSong(new Song("Imagine", "John Lennon", 187));
+        playlist.addSong(new Song("Smells Like Teen Spirit", "Nirvana", 301));
+
+        System.out.println("=== " + playlist.getName() + " ===");
+        System.out.println("Durata totală: " + playlist.getTotalDuration() + "s\n");
+
+        System.out.println("--- Sortate după titlu ---");
+        playlist.printSortedByTitle();
+
+        System.out.println("\n--- Sortate după durată ---");
+        playlist.printSortedByDuration();
     }
 }
